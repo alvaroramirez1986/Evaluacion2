b707e2f1471f5c9e35b2366a475560048480fda6
alvaroramirez1986 <under.midnatt@hotmail.com>
2025-11-12 23:31:28 +0000

    Commit inicial de scripts geolocalización y 50libros

---
+++ Ramdom50libros.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from faker import Faker
+
+APIHOST = "http://library.demo.local"
+LOGIN = "cisco"
+PASSWORD = "Cisco123!"
+
+def getAuthToken():
+    authCreds = (LOGIN, PASSWORD)
+    r = requests.post(
+        f"{APIHOST}/api/v1/loginViaBasic",
+        auth = authCreds
+    )
+    
+    if r.status_code == 200:
+        return r.json()["token"]
+    else:
+        raise Exception(f"Status code {r.status_code} and text {r.text}, while trying to Auth.")
+
+def addBook(book, apiKey):
+    r = requests.post(
+        f"{APIHOST}/api/v1/books",
+        headers = {
+            "Content-type": "application/json",
+            "X-API-Key": apiKey
+        },
+        data = json.dumps(book)
+    )
+    
+    if r.status_code == 200:
+        print(f"Book {book} added.")
+    else:
+        raise Exception(f"Error {r.status_code}  en {r.text}, Intentando agregar libro {book}.")
+
+apiKey = getAuthToken ()
+fake = Faker()
+
+print(f"--- Agregando libros {APIHOST} ---")
+
+for i in range(10, 59):
+    
+    fakeTitle = fake.catch_phrase()
+    fakeAuthor = fake.name()
+    fakeISBN = fake.isbn13()
+    
+    book = {"id": i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+    
+    addBook(book, apiKey)
+
+print("--- Listo. ---")
